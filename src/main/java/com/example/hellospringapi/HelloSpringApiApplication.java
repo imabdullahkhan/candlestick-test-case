bbc049e0b38e69f4ae6206f5228a720c2a1c932f
Abdullah Khan <mabdullahkhan1996@gmail.com>
2026-04-07 21:07:07 +0500

    Project init

---
+++ src/main/java/com/example/hellospringapi/HelloSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellospringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hellospringapi/HelloSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellospringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
